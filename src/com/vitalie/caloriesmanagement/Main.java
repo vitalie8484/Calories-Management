98ebcd1cfa1a346c62b892ca2b1f469264977ea3
Vitalie_Notebook <vitalie.jurjiu@gmail.com>
2018-03-04 22:59:59 -0500

    3/4/2018 - Converted to web application

--- src/com/vitalie/caloriesmanagement/Main.java
+++
@@ -1,9 +0,0 @@
-package com.vitalie.caloriesmanagement;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Calories Management Project");
-    }
-}
---
+++ src/main/java/com/vitalie/caloriesmanagement/web/UserServlet.java
@@ -0,0 +1,16 @@
+package com.vitalie.caloriesmanagement.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        //request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp");
+    }
+}
