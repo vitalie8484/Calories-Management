785c4ca5a2f392b2424a359c5b50918bb257e830
Vitalie_Notebook <vitalie.jurjiu@gmail.com>
2018-03-06 21:35:48 -0500

    3/6/2018 - Added Logging (slf4j & logback)

--- src/main/java/com/vitalie/caloriesmanagement/web/UserServlet.java
+++ src/main/java/com/vitalie/caloriesmanagement/web/UserServlet.java
@@ -1,5 +1,8 @@
 package com.vitalie.caloriesmanagement.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +11,12 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet
 {
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        log.debug("Redirect to userList");
+
         //request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
